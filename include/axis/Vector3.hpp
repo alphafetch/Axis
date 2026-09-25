9de4fdc4c8c2734c187d92c196641c176608829d
theTacocat88 <eliasglick1@gmail.com>
2026-09-24 21:06:22 -0400

    feat: implement move constructor, copy constructor, and arithmetic operators in Vector3 class

--- include/axis/Vector3.hpp
+++ include/axis/Vector3.hpp
@@ -4,7 +4,27 @@
 namespace axis {
     class Vector3 {
     public:
-        float x, y, z;  
+        float x, y, z; 
+        
+        Vector3(Vector3&& other) noexcept : x(other.x), y(other.y), z(other.z) {}
+        Vector3(const Vector3& other) : x(other.x), y(other.y), z(other.z) {}
+        Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
+        Vector3() : x(0), y(0), z(0) {}
+
+        Vector3& operator+(Vector3& other) {
+            Vector3 v(this->x + other.x, this->y + other.y, this->z + other.z);
+            return v;
+        }
+
+        Vector3& operator-(Vector3& other) {
+            Vector3 v(this->x - other.x, this->y - other.y, this->z - other.z);
+            return v;
+        }
+
+        Vector3& operator*(float scalar) {
+            Vector3 v(this->x * scalar, this->y * scalar, this->z * scalar);
+            return v;
+        }
     };
 }
 
