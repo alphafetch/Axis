cbd3bed16d77bb5f7e6a95da563e47a150884e6f
theTacocat88 <eliasglick1@gmail.com>
2026-09-24 21:03:22 -0400

    feat: implement move constructor, copy constructor, and arithmetic operators in Vector2 class

--- include/axis/Vector2.hpp
+++ include/axis/Vector2.hpp
@@ -5,6 +5,26 @@ namespace axis {
     class Vector2 {
     public:
         float x, y;
+
+        Vector2(Vector2&& other) noexcept : x(other.x), y(other.y) {}
+        Vector2(const Vector2& other) : x(other.x), y(other.y) {}
+        Vector2(float x, float y) : x(x), y(y) {}
+        Vector2() : x(0), y(0) {}
+
+        Vector2& operator+(Vector2& other) {
+            Vector2 v(this->x + other.x, this->y + other.y);
+            return v;
+        }
+
+        Vector2& operator-(Vector2& other) {
+            Vector2 v(this->x - other.x, this->y - other.y);
+            return v;
+        }
+
+        Vector2& operator*(float scalar) {
+            Vector2 v(this->x * scalar, this->y * scalar);
+            return v;
+        }
     };
 }
 
