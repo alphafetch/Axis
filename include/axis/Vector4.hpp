082a20f91718bc5e1606b7c848080a56f82a5445
theTacocat88 <eliasglick1@gmail.com>
2026-09-24 21:10:23 -0400

    feat: implement Vector4  constructors and arithmetic operators

--- include/axis/Vector4.hpp
+++ include/axis/Vector4.hpp
@@ -5,6 +5,26 @@ namespace axis {
     class Vector4 {
     public:
         float x, y, z, w;
+        
+        Vector4(Vector4&& other) noexcept : x(other.x), y(other.y), z(other.z), w(other.w) {}
+        Vector4(const Vector4& other) : x(other.x), y(other.y), z(other.z), w(other.w) {}
+        Vector4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+        Vector4() : x(0), y(0), z(0), w(0) {}
+
+        Vector4& operator+(Vector4& other) {
+            Vector4 v(this->x + other.x, this->y + other.y, this->z + other.z, this->w + other.w);
+            return v;
+        }
+
+        Vector4& operator-(Vector4& other) {
+            Vector4 v(this->x - other.x, this->y - other.y, this->z - other.z, this->w - other.w);
+            return v;
+        }
+
+        Vector4& operator*(float scalar) {
+            Vector4 v(this->x * scalar, this->y * scalar, this->z * scalar, this->w * scalar);
+            return v;
+        }
     };
 }
 
